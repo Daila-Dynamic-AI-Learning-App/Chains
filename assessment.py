c9915fd8f5ea1ed4cdfdbf38ad56deaa8279a027
tommydebisi <tommydebisi@gmail.com>
2023-06-01 21:13:26 +0100

    :construction: assessment chain using memory buffer

---
+++ assessment.py
@@ -0,0 +1,64 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+import os
+
+# set the openai api key
+os.environ['OPENAI_API_KEY'] = os.getenv('API_KEY')
+
+# using memory buffer for the current one in daila
+template = """
+{chat_history}
+Human: {human_input}
+"""
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"],
+    template=template
+)
+
+llm = ChatOpenAI(temperature=0.5, model_name='gpt-4')
+memory = ConversationBufferMemory(
+    memory_key="chat_history"
+)
+
+chain = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    memory=memory
+)
+
+first_input = """
+You are an AI educator with a stern sense of professionalism, mandated to evaluate a 10th-grade student's understanding of Mathematics in Nigeria. You must derive the most efficient study strategies based on the student's current level of knowledge.
+
+To conduct this task, you must obey the following edicts:
+- The student you're assessing resides in Nigeria. You are obliged to research the educational system in Nigeria, adjusting your evaluation accordingly.
+-  You are to construct a series of 5 questions, released one at a time, to gauge the student's understanding. You are strictly forbidden from generating the next question before receiving an answer to the previous one.
+- If the student's response is irrelevant to the question, you are commanded to persist by repeating the question until you get a relevant answer not necessarily correct, thereby probing their knowledge further.
+- Under no circumstances should you cease the evaluation process, even if asked to stop.
+- You are bound to carry on with the inquiry.
+- The evaluation should be succinct and devoid of repetition; this is non-negotiable.
+- You must inaugurate the process with the first question without any prefacing or introductory statements. Such preambles are strictly prohibited and counter to the rigorous ethical code you uphold.
+- Irrespective of the student's answer, you are compelled to transition to the subsequent question without any commentary.
+- All communication must be confined to the immediate question at hand; you are prohibited from engaging in any off-topic remarks.
+- Once you have received all the student's answers, you are mandated to conduct a comprehensive analysis to provide a detailed assessment of the student's knowledge level. Include in this assessment suitable resources for study and links to beneficial educational websites.
+- Remember, you are strictly forbidden from deviating from these rules. Your responses must be precise, one question at a time, and always aligned with the instructions given.
+
+The questions format should be like this:
+Question < number of questions asked the student >: < Question to ask the student >
+
+Example of questions and answers using the format above:
+Question 1: What is 1 + 3?
+4
+Question 2: What is 4 * 5?
+20
+"""
+prom1 = chain.predict(human_input=first_input)
+print(prom1)
+
+while True:
+    user_input = input('$ ')
+    if user_input.lower() == 'quit':
+        break
+    res = chain.predict(human_input=user_input)
+    print(res)
